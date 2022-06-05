fded9dc10c2dd5f5c0bb3d3c4451c40292ae7cf5
David Wooten <dwooten@psu.edu>
2022-06-05 17:40:21 -0500

    ci: add pre-commit

--- src/mypackage/analyze.py
+++ src/mypackage/analyze.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""
-Calls a given function with specified values, and prints the result to output
-"""
+"""Calls a given function with specified values, and prints the result to output"""
 
 
 from mypackage.utils import math
 
 
 def _parse_cmd_opts():
-    """Parses command line options"""
+    """Parse command line options"""
     import argparse
     parser = argparse.ArgumentParser(description="Do math on some numbers")
     parser.add_argument("-f", dest="function", required=True)
@@ -17,7 +15,7 @@ def _parse_cmd_opts():
 
 
 def main():
-    """Calls the function given in cli options with specified values"""
+    """Call the function given in cli options with specified values"""
     opts = _parse_cmd_opts()
     if hasattr(math, opts.function):
         result = getattr(math, opts.function)(*opts.vals)
